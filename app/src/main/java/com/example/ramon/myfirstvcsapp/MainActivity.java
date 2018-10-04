47a7436a8e208f04e940bba93b8d85ef772ef4f6
Ramon Peart <ramon.peart@gmail.com>
2018-10-04 14:04:16 -0400

    Updated onOptionItemSelected Method in Main Activity.

--- app/src/main/java/com/example/ramon/myfirstvcsapp/MainActivity.java
+++ app/src/main/java/com/example/ramon/myfirstvcsapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,16 +38,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_git) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_git:
+                Toast.makeText(this, "Git", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_github:
+                Toast.makeText(this, "Git Hub", Toast.LENGTH_LONG).show();
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
